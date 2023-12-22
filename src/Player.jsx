55bdcc5437cac3e7c8edc78e8adc4ad37be29aef
petert40 <petertang224@gmail.com>
2023-12-22 14:14:18 -0500

    Add changes as directed in the exercises

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,25 +1,48 @@
 import React from "react";
 import { useState } from "react";
-function Player() {
+
+function Player({name, handleDelete}) {
+  let [score, setScore] = useState(0)
+
+  function addPoint(){
+    setScore(() => score + 1)
+  }
+
+  function subtractPoint(){
+    if(score >= 1){
+      setScore(() => score - 1);
+    }
+  }
+
   return (
     <div className="container-fluid border border-dark p-3 m-3">
       <div className="row justify-content-center">
         {/* Render Name here */}
-        <h2 className="text-center"></h2>
+        <h2 className="text-center">{name}</h2>
       </div>
       <div className="row justify-content-center">
-        <p className="text-center fs-3">{/* Render score here */}</p>
+        <p className="text-center fs-3">{score}</p>
       </div>
       <div className="row justify-content-center">
         <div className="col-md-6">
           {/* Add increment event handler */}
-          <button className="btn btn-outline-primary w-100">Add Point +</button>
+          <button onClick={addPoint} className="btn btn-outline-primary w-100">Add Point +</button>
         </div>
         <div className="col-md-6">
           {/* Add decrement event handler */}
-          <button className="btn btn-outline-danger w-100">
+          <button onClick={subtractPoint} className="btn btn-outline-danger w-100">
             Remove Point -
           </button>
+        </div>
+        <div className="row">
+        <button
+              onClick={handleDelete}
+              class="btn btn-danger col-md-4 m-2"
+              type="button"
+              id="deletePlayer"
+            >
+              Delete 
+            </button>
         </div>
       </div>
     </div>
--- src/ScoreBoard.jsx
+++ src/ScoreBoard.jsx
@@ -3,7 +3,23 @@ import { useRef, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Player from "./Player";
 function ScoreBoard() {
- 
+  let [players, setPlayers] = useState([
+    {id:1, name: 'Jack',score:0},
+    {id:2, name: 'Daniel',score:0},
+    {id:3, name:'Ben',score:0}
+  ])
+  let playerRef = useRef();
+
+  function addPlayer(){
+    let name = playerRef.current.value || undefined;
+    setPlayers([...players, {id: Date.now(), name: name}])
+    playerRef.current.value = '';
+  }
+
+  function handleDelete(id){
+    setPlayers(players.filter(player => player.id != id));
+  }
+
   return (
     <div className="container">
       <div className="row  text-center">
@@ -14,6 +30,7 @@ function ScoreBoard() {
           <div class="input-group mb-3">
             {/* Modify input so that it is either connected to a ref or some kind of input state */}
             <input
+              ref = {playerRef}
               type="text"
               class="form-control"
               placeholder="New Player Name"
@@ -23,12 +40,14 @@ function ScoreBoard() {
             />
             {/* add Add Player event handling to this button */}
             <button
-              class="btn btn-outline-primary"
+              onClick={addPlayer}
+              className="btn btn-outline-primary"
               type="button"
               id="addPlayer"
             >
               Add Player
             </button>
+
           </div>
         </div>
       </div>
@@ -40,6 +59,7 @@ function ScoreBoard() {
               <Player
                 key={player.id}
                 name={player.name}
+                handleDelete = {() => handleDelete(player.id)}
                 // Anonymous arrow function that we can hold off activating
                 // until the user clicks a button inside of the Player component
               />
